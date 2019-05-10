181dc07fe400016e0f6dd8972012ab0505f8ac08
Sam Low-Chappell <sam@hackeryou.com>
2019-05-10 10:04:16 -0400

    Updating Index, JS, and styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
 $(function() {
     console.log('ready');
+
+
+    $('.questionOne:nth-child(1)').on('click', () => {
+        console.log("I clicked answer one!");
+    });
+
+    $('.questionOne:nth-child(2)').on('click', () => {
+        console.log("I clicked answer 2!");
+
+    });
+
+    $('.questionOne:nth-child(3)').on('click', () => {
+        console.log("I clicked answer 3!");
+
+    });
 });
